6d4cef6b989f4538c638a3ebb69f8a19aec67fa4
Dmitry Shechtman <damnian@gmail.com>
2017-07-12 18:11:52 +0300

    Add IInnerCameraWriter

---
+++ src/Net.Chdk.Meta.Writers.Camera.Core/IInnerCameraWriter.cs
@@ -0,0 +1,14 @@
+﻿using Net.Chdk.Meta.Model.Camera;
+using System.Collections.Generic;
+
+namespace Net.Chdk.Meta.Writers.Camera
+{
+    public interface IInnerCameraWriter<TCamera, TModel, TCard>
+        where TCamera : CameraData<TCamera, TModel, TCard>
+        where TModel : CameraModelData
+        where TCard : CardData
+    {
+        void WriteCameras(string path, IDictionary<string, TCamera> cameras);
+        string Extension { get; }
+    }
+}
